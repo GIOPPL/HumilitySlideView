c593ff4dd68b6d4e4606d04d963da6c40a53eb31
GIOPPL <1766468447@qq.com>
2020-06-19 11:42:07 +0800

    第三次修改

--- app/src/main/java/com/gioppl/humiditysliderview/SlideView.java
+++ app/src/main/java/com/gioppl/humiditysliderview/SlideView.java
@@ -24,9 +24,10 @@ public class SlideView extends View {
     private int divideNum;
     private int colorLineStart, colorLineEnd;
     private int colorMark;
-    private int colorText,colorTextSelect;
-    private int orientation;
-    private int markWidth;
+    private int colorText, colorTextSelect;
+    private boolean isRatio;//是否是百分号
+    private float normalMarkLength, specialMarkLength;//普通的刻度和为十的刻度的长度
+    private float markToLineMargin;//刻度和滑动线之间的距离
 
     private int colorButton;
     private Context context;
@@ -57,9 +58,28 @@ public class SlideView extends View {
         initAttrs(attrs);
     }
 
+    private void initAttrs(AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.SlideView);
+        maxNum = typedArray.getInt(R.styleable.SlideView_maxNum, 100);
+        minNum = typedArray.getInt(R.styleable.SlideView_minNum, 0);
+        divideNum = typedArray.getInt(R.styleable.SlideView_divideNum, 8);
+        colorLineStart = typedArray.getColor(R.styleable.SlideView_lineStartColor, Color.WHITE);
+        colorLineEnd = typedArray.getColor(R.styleable.SlideView_lineEndColor, Color.WHITE);
+        colorButton = typedArray.getColor(R.styleable.SlideView_buttonColor, Color.WHITE);
+        btnCircle.r = typedArray.getInt(R.styleable.SlideView_circleR, 100);
+        colorMark = typedArray.getColor(R.styleable.SlideView_markColor, Color.WHITE);
+        colorText = typedArray.getColor(R.styleable.SlideView_textColor, Color.WHITE);
+        colorTextSelect = typedArray.getColor(R.styleable.SlideView_colorTextSelect, Color.BLUE);
+        isRatio = typedArray.getBoolean(R.styleable.SlideView_isRatio, true);
+        normalMarkLength = typedArray.getFloat(R.styleable.SlideView_normalMarkLength, 50);
+        specialMarkLength = typedArray.getFloat(R.styleable.SlideView_specialMarkLength, 100);
+        markToLineMargin = typedArray.getFloat(R.styleable.SlideView_markToLineMargin, 50);
+        typedArray.recycle();
+    }
+
     private void drawLinePaths(Canvas canvas) {
         mPathLine = new Path();
-        mPathLine.moveTo(btnCircle.x, 0);
+        mPathLine.moveTo(btnCircle.x, -50);
         mPathLine.lineTo(btnCircle.x, btnCircle.y - btnCircle.r - btnCircle.r * 2);
         mPathLine.quadTo(btnCircle.x - btnCircle.r * 0.2f, btnCircle.y - btnCircle.r * 1.9f, btnCircle.x - btnCircle.r, btnCircle.y - btnCircle.r * 1.5f);
         mPathLine.quadTo(btnCircle.x - 2 * btnCircle.r, btnCircle.y - btnCircle.r * 0.9f, btnCircle.x - btnCircle.r * 2, btnCircle.y);
@@ -124,54 +144,64 @@ public class SlideView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-//        canvas.save();
-//        canvas.rotate(-90);
-//        canvas.translate(-getHeight(), 0);
-
+        canvas.translate(0, 50);
         canvas.drawCircle(btnCircle.x, btnCircle.y, btnCircle.r, mPaintButton);
         drawLinePaths(canvas);
         drawMark(canvas);
         drawText(canvas);
-
-//        canvas.restore();
     }
 
     private void drawText(Canvas canvas) {
         float resolutionRation = 100;
         int totalTextNum = divideNum;
         float everyTextHeight = height / totalTextNum;//每个十的整数的高度
-        float everyMarkHeight = everyTextHeight/10;//每个小的刻度的高度
+        float everyMarkHeight = everyTextHeight / 10;//每个小的刻度的高度
+        String normalTail;
+        String enlargeTail;
+        if (isRatio) {//是否带百分号
+            normalTail = "0%";
+            enlargeTail = "%";
+        } else {
+            normalTail = "0";
+            enlargeTail = "";
+        }
         for (int i = 0; i <= totalTextNum; i++) {
-            if (touchStatus == 0) {
-                canvas.drawText(i + "0%", 30, i * everyTextHeight, mPaintText);
+            if (touchStatus == 0) {//静止
+                canvas.drawText(handleText(i,normalTail), 30, i * everyTextHeight, mPaintText);
             } else if (touchStatus == 1) {//上滑动
                 if ((i * everyTextHeight) > btnCircle.y - resolutionRation && (i * everyTextHeight) < btnCircle.y + resolutionRation) {//正常绘制
                     //放大绘制
                     setTextPaintStyle(true);
-                    int g= (int) (btnCircle.y/everyMarkHeight);
-                    canvas.drawText(g+"%", 30, btnCircle.y, mPaintText);
+                    int g = (int) (btnCircle.y / everyMarkHeight);
+                    canvas.drawText(g + enlargeTail, 30, btnCircle.y, mPaintText);
                 } else {
                     //正常绘制
                     setTextPaintStyle(false);
-                    canvas.drawText(i + "0%", 30, i * everyTextHeight, mPaintText);
+                    canvas.drawText(handleText(i,normalTail), 30, i * everyTextHeight, mPaintText);
                 }
             } else {//下滑动
                 if ((i * everyTextHeight) > btnCircle.y - resolutionRation && (i * everyTextHeight) < btnCircle.y + resolutionRation) {//正常绘制
                     //放大绘制
                     setTextPaintStyle(true);
                     int g= (int) (btnCircle.y/everyMarkHeight);
-                    canvas.drawText(g+"%", 30, btnCircle.y, mPaintText);
+                    canvas.drawText(g + enlargeTail, 30, btnCircle.y, mPaintText);
                 } else {
                     //正常绘制
                     setTextPaintStyle(false);
-                    canvas.drawText(i + "0%", 30, i * everyTextHeight, mPaintText);
+                    canvas.drawText(handleText(i,normalTail), 30, i * everyTextHeight, mPaintText);
                 }
             }
 
         }
     }
+    private String handleText(int i,String tail){
+        String s=i+tail;
+        if (s.equals("00")||s.equals("00%")){
+            s=s.replaceFirst("00","0");
+        }
+        return s;
+    }
     private void drawMark(Canvas canvas) {
-        int markX = 100;//刻度和圆心的x距离
         int totalMarkNum = divideNum*10;//刻度总个数
         float everyMarkHeight = height / totalMarkNum;//每个的高度
         int a = 0;
@@ -182,11 +212,11 @@ public class SlideView extends View {
             pathMeasure.getPosTan(pathMeasure.getLength() / totalMarkNum * i, pos, tan);
             float x = pos[0];
             if (a != 5 && a != 10) {//一般的刻度
-                canvas.drawLine(x - markX, i * everyMarkHeight, x - markX + markWidth, i * everyMarkHeight, mPaintMark);
+                canvas.drawLine(x - markToLineMargin - normalMarkLength, i * everyMarkHeight, x -  markToLineMargin , i * everyMarkHeight, mPaintMark);
             } else if (a == 5) {
-                canvas.drawLine(x - markX, i * everyMarkHeight, x - markX + markWidth, i * everyMarkHeight, mPaintMark);
+                canvas.drawLine(x - markToLineMargin - normalMarkLength, i * everyMarkHeight, x -  markToLineMargin, i * everyMarkHeight, mPaintMark);
             } else {//=10
-                canvas.drawLine(x - markX - 50, i * everyMarkHeight, x - markX + markWidth, i * everyMarkHeight, mPaintMark);
+                canvas.drawLine(x - markToLineMargin - specialMarkLength, i * everyMarkHeight, x -  markToLineMargin , i * everyMarkHeight, mPaintMark);
                 a = 0;
             }
         }
@@ -225,23 +255,6 @@ public class SlideView extends View {
         return true;
     }
 
-    private void initAttrs(AttributeSet attrs) {
-        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.SlideView);
-        maxNum = typedArray.getInt(R.styleable.SlideView_maxNum, 100);
-        minNum = typedArray.getInt(R.styleable.SlideView_minNum, 0);
-        divideNum = typedArray.getInt(R.styleable.SlideView_divideNum, 8);
-        colorLineStart = typedArray.getColor(R.styleable.SlideView_lineStartColor, Color.WHITE);
-        colorLineEnd = typedArray.getColor(R.styleable.SlideView_lineEndColor, Color.WHITE);
-        colorButton = typedArray.getColor(R.styleable.SlideView_buttonColor, Color.WHITE);
-        btnCircle.r = typedArray.getInt(R.styleable.SlideView_circleR, 100);
-        markWidth = typedArray.getInt(R.styleable.SlideView_markWidth, 50);
-        colorMark = typedArray.getColor(R.styleable.SlideView_markColor, Color.WHITE);
-        colorText = typedArray.getColor(R.styleable.SlideView_textColor, Color.WHITE);
-        colorTextSelect = typedArray.getColor(R.styleable.SlideView_colorTextSelect, Color.BLUE);
-        orientation = 1;
-        typedArray.recycle();
-
-    }
 
     public interface ScrollCallBack {
         void scrollBack(int num);
